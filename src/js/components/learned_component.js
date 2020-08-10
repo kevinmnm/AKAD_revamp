0875de84c3869b4ecd17840050d2be4df25c3e21
Kevin Song <kevinmnm2010@gmail.com>
2020-08-10 15:53:33 -0400

    learned_section flex skeleton setup

--- src/js/components/learned_component.js
+++ src/js/components/learned_component.js
@@ -3,11 +3,11 @@ const templater = `
         <div class='learned_previous'>
 
         </div>
-        <div class='learned_next'>
-            
-        </div>
         <div class='learned_content'>
 
+        </div>
+        <div class='learned_next'>
+            
         </div>
     </div>
 `
