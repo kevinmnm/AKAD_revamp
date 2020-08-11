f2bc228c1bc05872a70799af3fdaf1c330b863cd
Kevin Song <kevinmnm2010@gmail.com>
2020-08-11 11:43:57 -0400

    fixed minor error

--- src/js/hover_function.js
+++ src/js/hover_function.js
@@ -1,10 +1,11 @@
+import AKAD from "./ADAKNotes_revamp.js";
 
 export default function custom_title(e){
     const el = document.querySelector('#hover_el');
-    el.style.top = e.clientY + 'px';
+    el.style.top = e.clientY - 50 + 'px';
     el.style.left = e.clientX - 100 + 'px';
 
-    el.innerHTML = this.akad[Number(e.target.innerHTML)-1].topic;
+    el.innerHTML = AKAD[Number(e.target.innerHTML)-1].topic;
     console.log(Number(e.target.innerHTML)-1);
 }
 
