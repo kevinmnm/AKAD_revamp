b2215ac002fba292d5ec44e4245ed49241e33f7f
Kevin Song <kevinmnm2010@gmail.com>
2020-08-12 15:15:04 -0400

    created function to stop animation at the end of content

--- src/js/components/learned_component.js
+++ src/js/components/learned_component.js
@@ -5,11 +5,11 @@ import AKAD from "../ADAKNotes_revamp.js";
 const templater = `
     <div class='learned_section animate__animated'>
 
-        <div class='learned_previous' @click='prev_content()'>
+        <div class='learned_previous' @click='prev_content($event)'>
             <div class='fa fa-chevron-circle-left'></div>
         </div>
     
-        <div class='learned_content' v-show='show_content'>
+        <div class='learned_content'>
             <code class='date'>{{ akad[pv_new].date }}</code>
             <div class='title'><u>{{ akad[pv_new].topic }}</u></div>
             <div class='description'>{{ akad[pv_new].description }}</div>
@@ -24,7 +24,7 @@ const templater = `
             </div>
         </div>
     
-        <div class='learned_next' @click='next_content()'>
+        <div class='learned_next' @click='next_content($event)'>
             <div class='fa fa-chevron-circle-right'></div>
         </div>
 
@@ -45,41 +45,50 @@ export default {
         }
     },
     methods: {
-        prev_content(){
-            if (this.parentValue === 0) {return}
+        prev_content(e){
             let dv = document.querySelector('.learned_content');
+            if (this.parentValue <= 0) {
+                dv.classList.remove('animate__bounceInRight');
+                this.parentValue = 0;
+                return
+            }
+            
 
-        if (dv.classList.contains('animate__bounceInRight')) {
-            dv.classList.remove('animate__bounceInRight');
-        }
+            if (dv.classList.contains('animate__bounceInRight')) {
+                dv.classList.remove('animate__bounceInRight');
+            }
 
-        dv.classList.add('animate__bounceOutRight');
-        setTimeout(()=>{
-            dv.classList.remove('animate__bounceOutRight');
-            dv.classList.add('animate__bounceInLeft');
-            this.parentValue--;
-        },200);
+            dv.classList.add('animate__bounceOutRight');
+            setTimeout(()=>{
+                dv.classList.remove('animate__bounceOutRight');
+                dv.classList.add('animate__bounceInLeft');
+                this.parentValue--;
+            },200);
         },
-        next_content(){
-            if (this.parentValue === this.akad.length - 1) {return}
-        let dv = document.querySelector('.learned_content');
-    
-        if (dv.classList.contains('animate__bounceInLeft')) {
-            dv.classList.remove('animate__bounceInLeft');
-        }
+        next_content(e){
+            let dv = document.querySelector('.learned_content');
+            if (this.parentValue >= this.akad.length - 1) {
+                dv.classList.remove('animate__bounceInLeft');
+                this.parentValue = this.akad.length -1;
+                return
+            }
+            
+            if (dv.classList.contains('animate__bounceInLeft')) {
+                dv.classList.remove('animate__bounceInLeft');
+            }
 
-        dv.classList.add('animate__bounceOutLeft');
-        setTimeout(()=>{
-            dv.classList.remove('animate__bounceOutLeft');
-            dv.classList.add('animate__bounceInRight');
-            this.parentValue++;
-        },200);
+            dv.classList.add('animate__bounceOutLeft');
+            setTimeout(()=>{
+                dv.classList.remove('animate__bounceOutLeft');
+                dv.classList.add('animate__bounceInRight');
+                this.parentValue++;
+            },200);
         }
     },
     watch: {
         pv_new(){
-            if (this.pv_new !== null) {
-                this.show_content = true;
+            if (this.pv_new < 0) {
+                this.pv_new = 0;
             }
         }
     }
