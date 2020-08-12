60318f454911f9c923ddce76bab59a243cdafcbe
Kevin Song <kevinmnm2010@gmail.com>
2020-08-12 12:57:20 -0400

    exporting sort function for DRY code

--- src/js/components/list_component.js
+++ src/js/components/list_component.js
@@ -1,27 +1,26 @@
 import AKAD from "../ADAKNotes_revamp.js";
 import custom_title, { custome_title_hide } from "../hover_function.js";
+import sort from "../sort_function.js";
 
 const templater = `
     <div id='list_wrapper'>
         <div class='list_all'>All Contents
         <div class="fa fa-sort" @click='sort_icon = !sort_icon'></div>
-        <div class='sort_by' @click.capture='sort($event)' v-show='sort_icon'>
+        <div class='sort_by' @click.capture='sort_import($event)' v-show='sort_icon'>
             <div :class='{js_select: js_selected}'>JavaScript</div>
             <div :class='{vue_select: vue_selected}'>Vue</div>
             <div :class='{css_select: css_selected}'>CSS</div>
         </div>
         </div>
-
             <div 
             @animationend='end()'
-            class='list_block' 
+            class='list_block animate__animated animate__flipInY' 
             v-for='(all,ind) in akad' 
             @click='emitter($event)' 
             @mousemove='hover_func($event)'
             @mouseleave='hover_func2($event)'>
                 {{ ind + 1 }}
             </div>
-
     <div id='hover_el'></div>
     </div>
 `
@@ -46,52 +45,14 @@ export default {
         hover_func(e){
             custom_title(e); //Imported from another file.
         },
-        hover_func2(e){
-            custome_title_hide(e);
+        hover_func2(e){ 
+            custome_title_hide(e); //Imported from another file.
         },
         emitter(e){ //Emits block's innerHTML number.
             this.$emit('learn-emit', Number(e.target.innerHTML));
         },
-        sort(e){
-            let x = e.target;
-            let js_blocks = document.querySelectorAll('.js');
-            let vue_blocks = document.querySelectorAll('.vue');
-            let css_blocks = document.querySelectorAll('.css');
-
-            if (x.innerHTML === 'JavaScript') { //Toggle js.
-                this.js_selected = !this.js_selected; //Remove shining class.
-                if (this.js_selected) {
-                    for (let y of js_blocks) {
-                        y.style.display = 'block';
-                    }
-                } else {
-                    for (let y of js_blocks) {
-                        y.style.display = 'none';
-                    }
-                }
-            } else if (x.innerHTML === 'Vue') { //Toggle vue.
-                this.vue_selected = !this.vue_selected; //Remove shining class.
-                if (this.vue_selected) {
-                    for (let y of vue_blocks) {
-                        y.style.display = 'block';
-                    }
-                } else {
-                    for (let y of vue_blocks) {
-                        y.style.display = 'none';
-                    }
-                }
-            } else if (x.innerHTML === 'CSS') { //Toggle css.
-                this.css_selected = !this.css_selected; //Remove shining class.
-                if (this.css_selected) {
-                    for (let y of css_blocks) {
-                        y.style.display = 'block';
-                    }
-                } else {
-                    for (let y of css_blocks) {
-                        y.style.display = 'none';
-                    }
-                }
-            }
+        sort_import(e){
+            sort(e); //Imported from another file.
         },
         end(){
 
